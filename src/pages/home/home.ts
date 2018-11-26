1dd3ba227686cc2805901a2f09818b7b844659fb
canavar.toro <canavar.toro@gmail.com>
2018-11-26 21:02:48 +0300

    search bar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,78 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items: string[];
+  value = '';
 
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+    this.initializeItems();
+  }
+
+  cart(){
+    
+  }
+
+  showDetail(item: any) {
+    this.navCtrl.push('ListPage', {city: item});
+  }
+
+  initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+      'Buenos Aires',
+      'Cairo',
+      'Dhaka',
+      'Edinburgh',
+      'Geneva',
+      'Genoa',
+      'Glasglow',
+      'Hanoi',
+      'Hong Kong',
+      'Islamabad',
+      'Istanbul',
+      'Jakarta',
+      'Kiel',
+      'Kyoto',
+      'Le Havre',
+      'Lebanon',
+      'Lhasa',
+      'Lima',
+      'London',
+      'Los Angeles',
+      'Madrid',
+      'Manila',
+      'New York',
+      'Olympia',
+      'Oslo',
+      'Panama City',
+      'Peking',
+      'Philadelphia',
+      'San Francisco',
+      'Seoul',
+      'Taipeh',
+      'Tel Aviv',
+      'Tokio',
+      'Uelzen',
+      'Washington'
+    ];
+  }
+
+  getItems(q: string) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // if the value is an empty string don't filter the items
+    if (!q || q.trim() === '') {
+      return;
+    }
+
+    this.items = this.items.filter((v) => v.toLowerCase().indexOf(q.toLowerCase()) > -1);
+  }
+
+  openModal() {
+    let modal = this.modalCtrl.create('ListPage');
+    modal.present();
   }
 
 }
